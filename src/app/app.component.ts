462370324744791d106619f2ddf0360ba4600f15
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-02-03 15:18:33 +0530

    reactive form with validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-app';
+
+    personForm: FormGroup = this.fb.group({
+        name: ['', [Validators.required]],
+        age: [18, [Validators.required, Validators.min(18)]],
+        email: ['', [Validators.required, this.emailValidation()]]
+    });
+
+    result: string;
+
+    constructor(private fb: FormBuilder) { }
+
+    submitForm() {
+        console.log(this.personForm);
+        this.result = JSON.stringify(this.personForm.value);
+        this.personForm.reset();
+    }
+
+    emailValidation(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            // console.log(control.value);
+            let validateEmail = control?.value?.match(
+                /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+            if (validateEmail)
+                return null;
+            return { 'emailPattern': false};
+        }
+    }
 }
